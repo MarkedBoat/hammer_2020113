83d3f8a44fec9483383a92e7df9903a00c29ceac
kinglone <ahyjl@126.com>
2019-12-20 13:27:20 +0800

    计划任务 暴露=>web

---
+++ hammer/modules/eg/v1/api/console/ActionKill.php
@@ -0,0 +1,29 @@
+<?php
+
+    namespace modules\eg\v1\api\project;
+
+    use console\system\CmdLauncher;
+    use models\common\ActionBase;
+    use models\common\sys\Sys;
+
+
+    class ActionKillTask extends ActionBase {
+        public static function getClassName() {
+            return __CLASS__;
+        }
+
+        public function __construct($param = []) {
+            parent::init($param);
+        }
+
+        public function run() {
+            $planId = $this->params->getStringNotNull('planId');
+            return CmdLauncher::getPlanRunning($planId);
+            //return [$project, $branch];
+            //  $taskFile = Sys::app()->params['console']['logDir'] . '/project/task/' . $fileName;
+            // $logFile  = Sys::app()->params['console']['logDir'] . '/project/log/' . $fileName . '.log';
+            return Sys::app()->params['console']['tasks'];
+
+        }
+
+    }
---
+++ hammer/modules/eg/v1/api/console/ActionTasks.php
@@ -0,0 +1,27 @@
+<?php
+
+    namespace modules\eg\v1\api\project;
+
+    use models\common\ActionBase;
+    use models\common\sys\Sys;
+
+
+    class ActionTasks extends ActionBase {
+        public static function getClassName() {
+            return __CLASS__;
+        }
+
+        public function __construct($param = []) {
+            parent::init($param);
+        }
+
+        public function run() {
+
+            //return [$project, $branch];
+            //  $taskFile = Sys::app()->params['console']['logDir'] . '/project/task/' . $fileName;
+            // $logFile  = Sys::app()->params['console']['logDir'] . '/project/log/' . $fileName . '.log';
+            return Sys::app()->params['console']['tasks'];
+
+        }
+
+    }
