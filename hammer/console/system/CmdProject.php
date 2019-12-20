4d8574ca4b70bf7624c40545c4f4c54b0519fa1f
kinglone <ahyjl@126.com>
2019-12-20 11:07:59 +0800

    /hammer system/project scan --env=debug

---
+++ hammer/console/system/CmdProject.php
@@ -0,0 +1,47 @@
+<?php
+
+    /**
+     * Created by PhpStorm.
+     * User: markedboat
+     * Date: 2018/7/20
+     * Time: 11:01
+     */
+
+    namespace console\system;
+
+    use models\common\CmdBase;
+    use models\common\sys\Sys;
+
+    class CmdProject extends CmdBase {
+
+
+        public static function getClassName() {
+            return __CLASS__;
+        }
+
+
+        public function init() {
+            parent::init();
+        }
+
+
+        public function scan() {
+            echo "\n fuc\n";
+            $taskDir = self::$logDir = Sys::app()->params['console']['logDir'] . '/project/task';
+            $logDir  = self::$logDir = Sys::app()->params['console']['logDir'] . '/project/log';
+            foreach ([$taskDir, $logDir] as $dir)
+                if (!file_exists($dir)) {
+                    //exec("touch {$logFileName}");
+                    exec("mkdir {$dir}");
+                    exec("chmod 777 {$dir}");
+                };
+
+            $timeout = $this->params->getIntNotNull('timeLimit');
+            while ($timeout > 7000) {
+                $files=array_slice(scandir($taskDir),2);
+                var_dump($files);
+                sleep(10);
+            }
+
+        }
+    }
