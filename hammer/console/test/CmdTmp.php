39f82ebadfb38d1a35b2046213930756e063376e
kinglone <ahyjl@126.com>
2020-02-03 17:44:27 +0800

    放开

--- hammer/console/test/CmdTmp.php
+++ hammer/console/test/CmdTmp.php
@@ -30,6 +30,8 @@
             $redis->connect('redis-node02', 6389);
             $redis->auth('Ab-18upTxsmuzsf');
             $r=$redis->info();
+            echo "\n" . json_encode($r, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+
             var_dump($r);die;
             $iterator = null;
             $i        = 1;
