a733f5e1af0f752ed12380c481131dafd80fffa8
kinglone <ahyjl@126.com>
2020-02-04 16:04:54 +0800

    debug

--- hammer/console/test/CmdTmp.php
+++ hammer/console/test/CmdTmp.php
@@ -23,13 +23,15 @@
         public function test() {
             echo "\nok\n";
 
-            $key   = [
-                'bin' => ['host' => 'redis-node02', 'port' => 6389, 'password' => 'Ab-18upTxsmuzsf'], 'pay' => ['host' => 'redis-node03', 'port' => 6389, 'password' => 'on4PshJqmibi^2n'], 'mpr' => ['host' => 'redis_mpr', 'port' => 6389, 'password' => 'Cipa6hd0ev^vkCh'],
+            $array = [
+                'bin' => ['host' => 'redis-node02', 'port' => 6389, 'password' => 'Ab-18upTxsmuzsf'],
+                'pay' => ['host' => 'redis-node03', 'port' => 6389, 'password' => 'on4PshJqmibi^2n'],
+                'mpr' => ['host' => 'redis_mpr', 'port' => 6389, 'password' => 'Cipa6hd0ev^vkCh'],
             ];
             $redis = new \Redis();
-            $redis->connect('redis-node02', 6389);
-            $redis->auth('Ab-18upTxsmuzsf');
-            $redis->select(10);
+            //   $redis->connect('redis-node02', 6389);
+            //   $redis->auth('Ab-18upTxsmuzsf');
+            //   $redis->select(10);
             /*
             $r=$redis->info();
             echo "\n" . json_encode($r, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
@@ -37,18 +39,34 @@
             var_dump($r);
             var_dump($redis->dbSize());
             die;*/
-
-            $iterator = null;
-            $i        = 1;
-            while (true) {
-                $keys = $redis->scan($iterator);
-                if ($keys === false) {//迭代结束，未找到匹配pattern的key
-                    return;
-                }
-                foreach ($keys as $key) {
-                    echo date('Y-m-d H:i:s', time()) . ">" . $key . "\n";
+            $i = 0;
+            foreach ($array as $redisName => $cfg) {
+                $countRedis = 0;
+                $redis      = new \Redis();
+                $redis->connect($cfg['host'], $cfg['port']);
+                $redis->auth($cfg['password']);
+                echo "\n" . json_encode($redis->info(), JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+                continue;
+                for ($db = 0; $db < 16; $db++) {
+                    $redis->select($db);
+                    $iterator = null;
+                    $countDb  = 0;
+                    while (true) {
+                        $keys = $redis->scan($iterator);
+                        if ($keys === false) {//迭代结束，未找到匹配pattern的key
+                            return;
+                        }
+                        foreach ($keys as $key) {
+                            echo "i:{$i}/time:" . date('Y-m-d H:i:s', time()) . "/redis:{$redisName}/count:{$countRedis}/db:{$db}/count:{$countDb}/key:{ $key}\n";
+                            $i++;
+                            $countRedis++;
+                            $countDb++;
+                        }
+                    }
                 }
             }
+
+
             echo "\nover\n";
         }
     }
